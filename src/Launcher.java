96d6ac214279df7734c0fdbf26933309ca8e6785
Lucas Mari <mari@et.esiea.fr>
2021-11-29 14:54:13 +0100

    fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
 
 public class Launcher {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome !");
         Scanner var = new Scanner(System.in);
         String Svar = var.next();
-        while(!Svar.equals("quit")){
-            System.out.println("Unknown command");
+        int nbr1 = 0, nbr2 = 1, nbr3=0, i, nbr;
+        while (!Svar.equals("quit")) {
+            if (Svar.equals("fibo")) {
+                nbr = var.nextInt();
+                for (i = 2; i < nbr+1; i++) {
+                    nbr3 = nbr1 + nbr2;
+                    nbr1 = nbr2;
+                    nbr2 = nbr3;
+                }
+                System.out.println("" + nbr3);
+            } else {
+                System.out.println("Unknown command");
+            }
             Svar = var.next();
         }
     }
