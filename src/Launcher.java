66e77ab7d131f53edeb2ca64a3c6d9df13de9001
Lucas Mari <mari@et.esiea.fr>
2021-11-29 15:55:12 +0100

    avancement P5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
+import java.nio.file.*;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome !");
         Scanner var = new Scanner(System.in);
         String Svar = var.next();
+        String path, content;
+        String[] contentTab;
+        Path filechemin;
         int nbr1 = 0, nbr2 = 1, nbr3=0, i, nbr;
         while (!Svar.equals("quit")) {
             if (Svar.equals("fibo")) {
@@ -17,7 +23,36 @@ public class Launcher {
                 System.out.println("" + nbr3);
                 nbr1 = 0;
                 nbr2 = 1;
-            } else {
+                //Src algo fibo : https://waytolearnx.com/2018/11/lalgorithme-de-fibonacci-en-java.html
+            }
+            if(Svar.equals("freq")){
+                path = var.next();
+                filechemin = Paths.get(path);
+                try{
+                    content = Files.readString(filechemin);
+                    content = content.replaceAll("\\.", " ");
+                    content = content.replaceAll("\\?", " ");
+                    content = content.replaceAll("\\!", " ");
+                    content = content.replaceAll("\\:", " ");
+                    content = content.replaceAll("\\;", " ");
+                    content = content.replaceAll("\\,", " ");
+                    content = content.replaceAll("\\'", " ");
+                    content = content.replaceAll("\n", " ");
+                    content = content.toLowerCase(Locale.ROOT);
+                    contentTab = content.split(" ");
+                    for(String word : contentTab){
+                        if(word.isBlank()) {
+                            word = null;
+                        }
+                        System.out.println(word);
+                    }
+                    //System.out.println(content);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else {
                 System.out.println("Unknown command");
             }
             Svar = var.next();
